9c8e0a9ea41ad3f493611b9b59a76397c072ac26
Fahim <fahim.muntasir2965@gmail.com>
2023-06-11 21:22:38 -0400

    yh

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,94 @@ async function fetchData() {
   let result = await fetch()
   console.log(result)
 }
+
+const links = [
+    'Home',
+    'Play List',
+    'Search',
+    'Updates',
+  ]
 </script>
 
 <template>
 <body>
   <nav>
-  <router-link to="/">Home</router-link> | 
+  <router-link to="/HomePage">Home</router-link> | 
   <router-link to="/PlayList">Play List</router-link> | 
-  <router-link to="/SignUp">Sign Up</router-link> |
+  <router-link to="/Search">Search</router-link> |
   <button @click="fetchData()">Fetch</button>
   </nav>
   <router-view />
   </body>
-</template>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar
+          class="me-10 ms-4"
+          color="grey-darken-1"
+          size="32"
+        ></v-avatar>
 
-<style scoped>
-body {
-  text-align: center;}
-</style>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          variant="text"
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-list-item-title>
+                    List Item {{ n }}
+                  </v-list-item-title>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  link
+                  color="grey-lighten-4"
+                >
+                  <v-list-item-title>
+                    Refresh
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
